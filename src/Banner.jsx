28208aa2549e781c7273a359af197e72c25bf1b0
Phan Binh <akiphan99@gmail.com>
2022-07-26 22:23:53 +0700

    modify

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -4,8 +4,8 @@ export default class Banner extends Component {
   render() {
     return (
       <div className='py-5'>
-        <div className='bg-light text-center p-4'>
-          <div className='p-4'>
+        <div className='bg-light text-center p-5'>
+          <div className='p-5'>
             <h1 className='display-5 fw-bold'>A warm welcome!</h1>
             <p className='fs-4'>
               Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create
--- src/Body.jsx
+++ src/Body.jsx
@@ -7,7 +7,7 @@ export default class Body extends Component {
     return (
       <main className='pt-5 container px-lg-5'>
         <Banner />
-        <div className='row'>
+        <div className='row pt-4'>
           <div className='col-xxl-4 col-lg-6 mb-5'>
             <Item />
           </div>
